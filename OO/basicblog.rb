280e776662d401216721af0d3a3fa2ae8459ae37
notarioR <notariorojo@gmail.com>
2015-01-02 18:06:43 +0100

    Blog

---
+++ OO/basicblog.rb
@@ -0,0 +1,34 @@
+class Blog
+
+	def initialize(title, date, text)
+		puts "Title for post"
+		@title = gets.chomp
+		puts "Date for post"
+		@date = gets.chomp
+		puts "Text for post"
+		@text = gets.chomp
+		puts "if the post is sponsored, you write 's' without '' "
+		@sponsor = gets.chomp
+	end
+
+	def front	
+		puts "  "
+		puts "Front Page Blog"
+		puts "---------------------"
+		if @sponsor == "s" 
+			puts "|" "  ++  #{@title}   ++  "
+		else
+			puts "|    #{@title}      "
+		end
+		puts "|    #{@date}       "
+		puts "|    #{@text}       "
+		puts "---------------------"
+	end
+
+end
+
+b = Blog.new(@title, @date, @text)
+# b2 = Blog.new(@title, @date, @text)
+
+b.front
+# b2.front
